4776d23da51ac417e1ffdb956592614f1fd9888a
Sean Landsman <sean@thelandsmans.com>
2020-01-20 11:09:57 +0000

    AG-3762 Framework Wrappers for Charts - Angular Wrapper

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,28 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {async, TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {AgChartsAngularModule} from "ag-charts-angular";
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                AppComponent
+            ],
+            imports: [
+                AgChartsAngularModule
+            ],
+        }).compileComponents();
+    }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
+    it('should create the app', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app).toBeTruthy();
+    });
 
-  it(`should have as title 'ag-charts-angular-example'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ag-charts-angular-example');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ag-charts-angular-example app is running!');
-  });
+    it(`should have as markerSize of 25`, () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app.markerSize).toEqual(25);
+    });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'ag-charts-angular-example';
+export class AppComponent implements AfterViewInit {
+    options = {};
+
+    markerSize = 25;
+
+    ngAfterViewInit(): void {
+        this.updateOptions()
+    }
+
+    setMarkerSize(markerSize: number) {
+        this.markerSize = markerSize;
+        this.updateOptions();
+    }
+
+    updateOptions() {
+        this.options = {
+            data: [{
+                month: 'Jan',
+                revenue: 155000,
+                profit: 33000
+            }, {
+                month: 'Feb',
+                revenue: 123000,
+                profit: 35500
+            }, {
+                month: 'Mar',
+                revenue: 172500,
+                profit: 41000
+            }, {
+                month: 'Apr',
+                revenue: 185000,
+                profit: 50000
+            }],
+            series: [{
+                xKey: 'month',
+                yKey: 'revenue'
+            }],
+            legend: {
+                markerSize: this.markerSize
+            }
+        };
+    }
 }
